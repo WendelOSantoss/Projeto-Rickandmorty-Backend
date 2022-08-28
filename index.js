83c1d702aa101c10e7f8a7b4a010f9be576a7a9b
Wendel Santos <wendelosantos@hotmail.com>
2022-08-28 16:57:39 -0300

    Adicionando middlewares e swagger

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use(cors());
 app.use("/rickandmorty", routes);
 
+
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
--- src/routes/rickandmorty.route.js
+++ src/routes/rickandmorty.route.js
@@ -1,13 +1,35 @@
-const route = require('express').Router();
-const controllerrickandmorty = require('../controllers/rickandmorty.controller');
+const route = require("express").Router();
+const controllerrickandmorty = require("../controllers/rickandmorty.controller");
+const {
+  validId,
+  validObjectBody,
+} = require("../middlewares/rickandmorty.middleware");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("../../swagger.json");
 
-
-
-route.get('/characters',controllerrickandmorty.findAllcharactersController);
-route.get('/characters/find/{id}',controllerrickandmorty.findByIdcharactersController);
-route.post('/characters/create/{id}', controllerrickandmorty.createcharactersController);
-route.put('/characters/update/{id}', controllerrickandmorty.updatecharactersController);
-route.delete('/characters/delete/{id}', controllerrickandmorty.deletecharactersController);
- 
+route.use("/api-docs", swaggerUi.serve);
+route.get("/api-docs", swaggerUi.setup(swaggerDocument));
+route.get("/characters", controllerrickandmorty.findAllcharactersController);
+route.get(
+  "/characters/find/:id",
+  validId,
+  controllerrickandmorty.findByIdcharactersController
+);
+route.post(
+  "/characters/create/:id",
+  validObjectBody,
+  controllerrickandmorty.createcharactersController
+);
+route.put(
+  "/characters/update/:id",
+  validId,
+  validObjectBody,
+  controllerrickandmorty.updatecharactersController
+);
+route.delete(
+  "/characters/delete/:id",
+  validId,
+  controllerrickandmorty.deletecharactersController
+);
 
 module.exports = route;
