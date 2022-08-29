ca83581e8dd47658db9fa3589a3be26174bd43f4
Wendel Santos <wendelosantos@hotmail.com>
2022-08-28 22:44:00 -0300

    Novo user controller and service

--- src/users/users.controller.js
+++ src/users/users.controller.js
@@ -1,8 +1,32 @@
-const createUserController = async(req, res) => {
-    res.send({message: "Create ok"})
-};
-const findallUserController = async(req, res) => {
-    res.send({message: "Find All ok"})
+const userService = require("./users.service");
+
+const createUserController = async (req, res) => {
+  const { nome, apelido, email, senha, foto } = req.body;
+
+  if (!nome || !apelido || !email || !senha || !foto) {
+    return res.status(400).send({ message: "Alguns campos estão faltando." });
+  }
+
+  const foundUser = await userService.findByEmailUserService(email);
+
+  if (foundUser) {
+    return res.status(400).send({ message: "Usuário já existe!" });
+  }
+
+  const user = await userService
+    .createUserService(req.body)
+    .catch((err) => console.log(err.message));
+
+  if (!user) {
+    return res.status(400).send({ message: "Erro ao criar usuário!" });
+  }
+
+
+res.status(201).send(user);
 };
 
-module.exports = {createUserController,findallUserController};
+const findallUserController = async (req, res) => {
+  res.send({ message: "Find All ok" });
+};
+
+module.exports = { createUserController, findallUserController };
